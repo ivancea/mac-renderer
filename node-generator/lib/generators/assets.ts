b4203e617f617af99fb974549ea992d44dab6018
Iván Cea Fontenla <ivancea96@outlook.com>
2023-03-18 12:21:49 +0100

    Fixed eslint and added assets

---
+++ node-generator/lib/generators/assets.ts
@@ -0,0 +1,10 @@
+export const assets = {
+  githubIcon: "./images/github-icon.svg",
+  highlightDefaultIcon: "./images/highlight-default-icon.svg",
+  jobDefaultIcon: "./images/job-default-icon.svg",
+  linkIcon: "./images/link-icon.svg",
+  linkedinIcon: "./images/linkedin-icon.svg",
+  locationIcon: "./images/location-icon.svg",
+  projectDefaultIcon: "./images/project-default-icon.svg",
+  twitterIcon: "./images/twitter-icon.svg",
+};
