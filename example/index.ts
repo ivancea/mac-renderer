18637d54b53d39267f4897d6992c80c3bb3ac2e6
Iván Cea Fontenla <ivancea96@outlook.com>
2023-03-19 11:27:12 +0100

    Added PDF example

--- example/index.ts
+++ example/index.ts
@@ -1,13 +1,21 @@
 import { promises as fs } from "fs";
+import { generatePdf } from "html-pdf-node";
 import { ManfredAwesomicCV, generateHtml } from "mac-renderer";
 
 async function main() {
   const rawCv = await fs.readFile("../CV/MAC.json");
-  const cv = JSON.parse(rawCv.toString()) as ManfredAwesomicCV;
+  const cv: ManfredAwesomicCV = JSON.parse(rawCv.toString());
 
   const html = await generateHtml(cv);
 
   await fs.writeFile("index.html", html);
+
+  const pdf = await (generatePdf(
+    { content: html },
+    { format: "A4", scale: 0.6 }
+  ) as unknown as Promise<Buffer>);
+
+  await fs.writeFile("index.pdf", pdf);
 }
 
 main();
