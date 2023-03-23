8074ebde4ad76092c582dd64fd75d7354cfdee46
Iván Cea Fontenla <ivancea96@outlook.com>
2023-03-23 22:53:50 +0100

    Generate CV in PDF

---
+++ action/index.ts
@@ -0,0 +1,30 @@
+import { promises as fs } from "fs";
+import { generatePdf } from "html-pdf-node";
+import { ManfredAwesomicCV, generateHtml } from "mac-renderer";
+import path from "path";
+
+async function main() {
+  const macPath = process.argv[2];
+  const outputPath = process.argv[3];
+
+  console.log(path.resolve(process.cwd(), macPath));
+  console.log(path.resolve(process.cwd(), outputPath));
+
+  const rawCv = await fs.readFile(path.resolve(process.cwd(), macPath));
+  const cv: ManfredAwesomicCV = JSON.parse(rawCv.toString());
+
+  // Generate the HTML
+  const html = await generateHtml(cv);
+
+  // Generate a PDF with the HTML
+  const pdf = await (generatePdf(
+    { content: html },
+    { format: "A4", scale: 0.6, printBackground: true }
+  ) as unknown as Promise<Buffer>);
+
+  await fs.mkdir(path.resolve(process.cwd(), outputPath), { recursive: true });
+  await fs.writeFile(path.resolve(process.cwd(), outputPath, "index.html"), html);
+  await fs.writeFile(path.resolve(process.cwd(), outputPath, "cv.pdf"), pdf);
+}
+
+main();
